69bcf59ee8f2d907251f1cbcd566edd6f88a9109
Cleanderson Lobo <cleandersonlobo@gmail.com>
2016-09-09 21:18:19 -0300

    bug fixes meteor 1.4.1+

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'cleandersonlobo:mdi-icons',
-  version: '1.1.1',
+  version: '1.1.2',
   summary: 'Material Design Icons font from Templarian (http://materialdesignicons.com/)',
   git: 'https://github.com/cleandersonlobo/mdi-icons.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@1.4');
+  
   api.addFiles([
     'MaterialDesign-MDI/css/materialdesignicons.css'
   ], 'client');
